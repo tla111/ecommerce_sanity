5892986ae31f48784244cc8dee33a2d2a63f7637
Tim La <tlamarketing111@gmail.com>
2022-05-02 11:22:25 -0400

    got rid of practice to follow to tutorial

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -12,29 +12,8 @@ export default function Home() {
   ]
 
   return (
-    // <div className={styles.container}>
     <>
-      <h1 style={{ color: "blue" }}>Hello World</h1>
-      <h2>E-Commerce Website Using Sanity</h2>
-      <h2 style={{ color: "orange" }}>Products:</h2>
-      <div style={{ border: "1px solid black" }}>
-        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-          <p>Product</p>
-          <p>Price</p>
-          <p>Availability</p>
-        </div>
-        <div>
-          {appleProducts.map((product) => (
-            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-              <p>{product.name}</p>
-              <p>{product.price}</p>
-              <p>{product.availability}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-      {/* <h1 style={{ color: "dodgerblue" }}>{appleProducts[0].name}</h1> */}
+      <h1>E-Commerce</h1>
     </>
-    // </div >
   )
 }
