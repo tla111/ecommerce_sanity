b401fb7d222a399371179eef2fbee1e76c234576
Tim La <tlamarketing111@gmail.com>
2022-05-05 11:12:01 -0400

    changed index to home

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const index = () => {
+const Home = () => {
   return (
-    <div>index</div>
+    <div>Home</div>
   )
 }
 
-export default index;
+export default Home;
