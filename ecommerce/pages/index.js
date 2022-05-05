567e050f55e0f03b1c1f57cb9028dd9832461455
Tim La <tlamarketing111@gmail.com>
2022-05-05 11:14:12 -0400

    created layout for index page

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      HeroBanner
+
+      <div>
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+
+      <div>
+        {["Product 1", "Product 2"].map((product) => (
+          product
+        ))}
+      </div>
+
+      Footer
+    </>
   )
 }
 
