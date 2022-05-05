49fe86db924cc5b1cd3e885f29b6b0a276426bb3
Tim La <tlamarketing111@gmail.com>
2022-05-05 11:08:38 -0400

    cleaned up index file, starting from scratch

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,19 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import React from 'react';
 
-export default function Home() {
-  const appleProducts = [
-    { name: "Macbook", price: "$1,000.00", availability: "In Stock" },
-    { name: "iPhone", price: "$500.00", availability: "In Stock" },
-    { name: "Air Pods", price: "$150.00", availability: "None" },
-    { name: "iPad", price: "$750.00", availability: "In Stock" },
-    { name: "Beats Headphones", price: "$100.00", availability: "None" },
-  ]
-
+const index = () => {
   return (
-    <>
-      <h1>E-Commerce</h1>
-    </>
+    <div>index</div>
   )
 }
+
+export default index;
