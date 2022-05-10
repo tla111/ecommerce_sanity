0e46af844ab3dd1663dc0149747cc32e0a70b01d
Tim La <tlamarketing111@gmail.com>
2022-05-10 14:42:42 -0400

    added props in home function

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -3,19 +3,19 @@ import React from 'react';
 import { client } from '../lib/client';
 import { Product, FooterBanner, HeroBanner } from '../components';
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
       <HeroBanner />
-
+      {console.log(bannerData)}
       <div className="products-heading">
         <h2>Best Selling Products in the World</h2>
         <p>Speakers of many variations</p>
       </div>
 
       <div className="products-container">
-        {["Product 1", "Product 2", "Product 3", "Product 4", "Product 5"].map((product) => (
-          product
+        {products?.map((product) => (
+          product.name
         ))}
       </div>
 
