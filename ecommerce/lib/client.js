ec8a6c5d39da82530be1ced6681d49d1116d3813
Tim La <tlamarketing111@gmail.com>
2022-05-10 14:28:48 -0400

    added values to sanity component

--- ecommerce/lib/client.js
+++ ecommerce/lib/client.js
@@ -2,9 +2,9 @@ import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 const client = sanityClient({
-    projectId: "",
-    dataset: "",
-    apiVersion: "",
+    projectId: "8w895cck",
+    dataset: "production",
+    apiVersion: "2022-05-10",
     useCdn: true,
     token: "",
 });
