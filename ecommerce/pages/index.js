2117a5bedefb9e7a34f3975cde3ef2aab8611011
Tim La <tlamarketing111@gmail.com>
2022-05-10 14:40:13 -0400

    created logic for getServerSideProps

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { client } from '../lib/client';
 import { Product, FooterBanner, HeroBanner } from '../components';
 
 const Home = () => {
@@ -21,6 +22,18 @@ const Home = () => {
       <FooterBanner />
     </>
   )
+}
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
 }
 
 export default Home;
