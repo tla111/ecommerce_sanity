1ed060a2761023bb311c490ea0b461289749a22e
Tim La <tlamarketing111@gmail.com>
2022-05-10 14:35:35 -0400

    save

--- ecommerce/lib/client.js
+++ ecommerce/lib/client.js
@@ -1,10 +1,14 @@
 import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
-const client = sanityClient({
+export const client = sanityClient({
     projectId: "8w895cck",
     dataset: "production",
     apiVersion: "2022-05-10",
     useCdn: true,
     token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 });
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
