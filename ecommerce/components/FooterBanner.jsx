964bfce223af03d7d7900dc4b5a4bd74ef6b35a8
Tim La <tlamarketing111@gmail.com>
2022-05-23 21:34:54 -0400

    creating layout for footer

--- ecommerce/components/FooterBanner.jsx
+++ ecommerce/components/FooterBanner.jsx
@@ -5,7 +5,16 @@ import { urlFor } from '../lib/client';
 
 const FooterBanner = ({ footerBanner }) => {
     return (
-        <div>FooterBanner</div>
+        <div className="footer-banner-container">
+            <div className="banner-desc">
+                <div className="left">
+
+                </div>
+                <div className="right">
+
+                </div>
+            </div>
+        </div>
     )
 }
 
