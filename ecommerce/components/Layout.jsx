5736473ab02ceac45b55dcea4c61abefaca5a13a
Tim La <tlamarketing111@gmail.com>
2022-05-23 21:48:26 -0400

    wrapped component in layout component

--- ecommerce/components/Layout.jsx
+++ ecommerce/components/Layout.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Head from 'next/head';
+
 import Navbar from './Navbar';
 import Footer from './Footer';
 
 
-const Layout = () => {
+const Layout = ({ children }) => {
     return (
         <div className="layout">
             <Head>
@@ -14,7 +15,7 @@ const Layout = () => {
                 <Navbar />
             </header>
             <main className="main-container">
-                EMPTY
+                {children}
             </main>
             <footer>
                 <Footer />
