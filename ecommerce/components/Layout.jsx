97a34f9484a1da04a5bbd0dc663a93ea984ce2de
Tim La <tlamarketing111@gmail.com>
2022-05-23 21:47:20 -0400

    set up layout component

--- ecommerce/components/Layout.jsx
+++ ecommerce/components/Layout.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import Head from 'next/head';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
 
 const Layout = () => {
     return (
-        <div>Layout</div>
+        <div className="layout">
+            <Head>
+                <title>JS Mastery Store</title>
+            </Head>
+            <header>
+                <Navbar />
+            </header>
+            <main className="main-container">
+                EMPTY
+            </main>
+            <footer>
+                <Footer />
+            </footer>
+        </div>
     )
 }
 
