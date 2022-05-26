6611b02dafa0efa6ed70bf4993d33b0c8ba86b9b
Tim La <tlamarketing111@gmail.com>
2022-05-25 22:04:15 -0400

    removed navbar word

--- ecommerce/components/Navbar.jsx
+++ ecommerce/components/Navbar.jsx
@@ -7,7 +7,6 @@ const Navbar = () => {
         <div className="navbar-container">
             <p >
                 {/* <Link href=""></Link> */}
-                Navbar
             </p>
         </div>
     )
