b6a07a3a05464c9c26b6bf46cf1233b77bf99e05
Tim La <tlamarketing111@gmail.com>
2022-05-28 10:40:04 -0400

    created shopping button

--- ecommerce/components/Navbar.jsx
+++ ecommerce/components/Navbar.jsx
@@ -5,9 +5,14 @@ import { AiOutlineShopping } from 'react-icons/ai';
 const Navbar = () => {
     return (
         <div className="navbar-container">
-            <p>
-                <Link href=""></Link>
+            <p className="logo">
+                <Link href="/">JSM Headphones</Link>
             </p>
+
+            <button type="button" className="cart-icon" onClick="">
+                <AiOutlineShopping />
+                <span className="cart-item-qty">1</span>
+            </button>
         </div>
     )
 }
