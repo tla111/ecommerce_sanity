1c09e5c293412ab20a14d6ae3a7b678a3ed8ae32
Tim La <tlamarketing111@gmail.com>
2022-05-28 10:51:56 -0400

    created logic to fetch product details from product

--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { client, urlFor } from '../../lib/client';
 
 const ProductDetails = () => {
     return (
-        <div>Product</div>
+        <div>
+            <div className="product-detail-container">
+                <div className="">
+                    <div className="image-container">
+                        <img src="" />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
+}
+
+export const getStaticProps = async () => {
+    const query = '*[_type == "product"]';
+    const products = await client.fetch(query);
+
+    const bannerQuery = '*[_type == "banner"]';
+    const bannerData = await client.fetch(bannerQuery);
+
+    return {
+        props: { products, bannerData }
+    }
 }
 
 export default ProductDetails
