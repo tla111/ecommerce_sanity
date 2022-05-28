cca5d498da5ee6384ac700fde65fb8a50a52da78
Tim La <tlamarketing111@gmail.com>
2022-05-28 10:58:52 -0400

    created logic for getstaticpaths

--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -4,17 +4,26 @@ import { client, urlFor } from '../../lib/client';
 const ProductDetails = ({ products, product }) => {
 
     const { image, name, details, price } = product;
+
     return (
         <div>
             <div className="product-detail-container">
                 <div className="">
                     <div className="image-container">
-                        <img src="" />
+                        <img src={urlFor(image && image[index])} />
                     </div>
                 </div>
             </div>
         </div>
     )
+}
+
+export const getStaticPaths = async () => {
+    const query = `*[_type == "product"] {
+        slug {
+            current
+        }
+    }`
 }
 
 export const getStaticProps = async ({ params: { slug } }) => {
@@ -23,6 +32,8 @@ export const getStaticProps = async ({ params: { slug } }) => {
 
     const product = await client.fetch(query);
     const products = await client.fetch([productsQuery]);
+
+    console.log(product);
 
     return {
         props: { products, product }
