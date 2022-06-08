f2b2843b6d3ab287d17ea85b239b696fa8f380e3
Tim La <tlamarketing111@gmail.com>
2022-06-08 08:38:06 -0400

    able to make products move

--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -77,7 +77,7 @@ const ProductDetails = ({ products, product }) => {
                 <div className="maylike-products-wrapper">
                     <h2>You may also like</h2>
                     <div className="marquee">
-                        <div className="maylike-products-container">
+                        <div className="maylike-products-container track">
                             {products.map((item) => (
                                 <Product key={item._id} product={item} />
                             ))}
