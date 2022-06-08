44179faee536c5e8338bc7dfd048996a1ce21fb3
Tim La <tlamarketing111@gmail.com>
2022-06-08 08:37:38 -0400

    added other products

--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import { client, urlFor } from '../../lib/client';
+import { Product } from '../../components';
 
 const ProductDetails = ({ products, product }) => {
 
@@ -69,6 +70,17 @@ const ProductDetails = ({ products, product }) => {
                             <button type="button" className="buy-now" onClick="">
                                 Buy Now
                             </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="maylike-products-wrapper">
+                    <h2>You may also like</h2>
+                    <div className="marquee">
+                        <div className="maylike-products-container">
+                            {products.map((item) => (
+                                <Product key={item._id} product={item} />
+                            ))}
                         </div>
                     </div>
                 </div>
