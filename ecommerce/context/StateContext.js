5402e521a0f99606bb930a003742101b92b754fe
Tim La <tlamarketing111@gmail.com>
2022-06-22 14:54:31 -0400

    created states to be used in projects

--- ecommerce/context/StateContext.js
+++ ecommerce/context/StateContext.js
@@ -4,5 +4,19 @@ import { toast } from 'react-hot-toast';
 const Context = createContext();
 
 export const StateContext = ({ children }) => {
+    const [showCart, setShowCart] = useState(false);
+    const [cartItems, setCartItems] = useState();
+    const [totalPrice, setTotalPrice] = useState();
+    const [totalQuantities, setTotalQuantities] = useState();
+    const [qty, setQty] = useState(1);
 
+    return (
+        <Context.Provider
+            value={{
+
+            }}
+        >
+            {children}
+        </Context.Provider>
+    )
 }
