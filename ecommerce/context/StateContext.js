247d49e2d2864e37353d14311f22ad4fbc24f1a4
Tim La <tlamarketing111@gmail.com>
2022-06-22 14:51:48 -0400

    created state context file

---
+++ ecommerce/context/StateContext.js
@@ -0,0 +1,8 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { toast } from 'react-hot-toast';
+
+const Context = createContext();
+
+export const StateContext = ({ children }) => {
+
+}
