48edce329d487eb560739fe9dfb3a2b6b868397b
Tim La <tlamarketing111@gmail.com>
2022-06-23 15:43:55 -0400

    destructured data from state context in navbar component

--- ecommerce/components/Navbar.jsx
+++ ecommerce/components/Navbar.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import Link from 'next/link';
 import { AiOutlineShopping } from 'react-icons/ai';
 
-import { Cart } from './'
+import { Cart } from './';
+import { useStateContext } from '../context/StateContext';
 
 const Navbar = () => {
+    const { showCart, setShowCart, totalQuantities } = useStateContext;
     return (
         <div className="navbar-container">
             <p className="logo">
