a7bd8dde59ac640d7f91dfd499c7ac2100a7c99a
Tim La <tlamarketing111@gmail.com>
2022-06-23 15:49:06 -0400

    imported imports for cart component

--- ecommerce/components/Cart.jsx
+++ ecommerce/components/Cart.jsx
@@ -1,4 +1,10 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import Link from 'next/link';
+import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
+import { TiDeleteOutline } from 'react-icons/ti';
+import toast from 'react-hot-toast';
+
+import { useStateContext } from '../context/StateContext';
 
 const Cart = () => {
     return (
